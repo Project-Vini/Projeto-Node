8573343b6053477481b367ee72685a97df3ab1cd
BrunoVinicius <brunoviniciustica@gmail.com>
2019-01-10 16:16:31 -0300

    Commit atualizado das Rotas Node.Js

--- app/routes/noticia.js
+++ app/routes/noticia.js
@@ -2,6 +2,7 @@ module.exports = function(app) {
 
     app.get('/noticia', function(req, res){
 
+        
         var connection = app.config.dbConnection();
         var noticiasModel = app.app.models.noticiasModel;
     
